824ecbc1058add7a473e2d616c43b6e724c5f272
hameetha <hb14@stir.ac.uk>
2021-03-25 20:10:59 +0400

    This is commit 2 times

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java! This is Modified Code");  
+     System.out.println("Hello Java! This is Modified Code2");  
      System.out.println("Welcome!");
     }  
 }  
